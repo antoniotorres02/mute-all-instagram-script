7068038781e93f432a938254050297de329a0422
antoniotorres02 <torresbordonantonio@gmail.com>
2023-10-24 22:34:18 +0200

    Added times between 50 users muted and recopilating data to avoid instagram block

--- script.js
+++ script.js
@@ -36,14 +36,13 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log('Hello World!');
+console.log('Starting script');
 require("dotenv/config");
 var instagram_private_api_1 = require("instagram-private-api");
 var ig = new instagram_private_api_1.IgApiClient();
 ig.state.generateDevice(process.env.IG_USERNAME);
-// Removed import statement for IgApiClient since it has already been imported in the first code block
 (function () { return __awaiter(void 0, void 0, void 0, function () {
-    var loggedInUser, followingFeed, myFollowing, _loop_1, _i, myFollowing_1, user;
+    var loggedInUser, followingFeed, myFollowing, i, myFollowing_1;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0: 
@@ -52,7 +51,6 @@ ig.state.generateDevice(process.env.IG_USERNAME);
             case 1:
                 //Login
                 _a.sent();
-                console.log(process.env.IG_PASSWORD); // Debugging statement
                 return [4 /*yield*/, ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD)];
             case 2:
                 loggedInUser = _a.sent();
@@ -62,41 +60,83 @@ ig.state.generateDevice(process.env.IG_USERNAME);
                         case 1: return [2 /*return*/, _a.sent()];
                     }
                 }); }); });
+                ig.challenge.auto(true);
                 followingFeed = ig.feed.accountFollowing(loggedInUser.pk);
                 return [4 /*yield*/, followingFeed.items()];
             case 3:
                 myFollowing = _a.sent();
-                _loop_1 = function (user) {
-                    var time;
-                    return __generator(this, function (_b) {
-                        switch (_b.label) {
-                            case 0: return [4 /*yield*/, ig.friendship.mutePostsOrStoryFromFollow({ targetPostsAuthorId: user.pk.toString(), targetReelAuthorId: user.pk.toString() })];
-                            case 1:
-                                _b.sent();
-                                console.log("Muted ".concat(user.username));
-                                time = Math.round(Math.random() * 6000) + 1000;
-                                return [4 /*yield*/, new Promise(function (resolve) { return setTimeout(resolve, time); })];
-                            case 2:
-                                _b.sent();
-                                return [2 /*return*/];
-                        }
-                    });
-                };
-                _i = 0, myFollowing_1 = myFollowing;
-                _a.label = 4;
+                return [4 /*yield*/, new Promise(function (resolve) { return setTimeout(resolve, 120000); })];
             case 4:
-                if (!(_i < myFollowing_1.length)) return [3 /*break*/, 7];
-                user = myFollowing_1[_i];
-                return [5 /*yield**/, _loop_1(user)];
+                _a.sent();
+                i = 0;
+                return [4 /*yield*/, muteFollower(myFollowing, ig, i)];
             case 5:
                 _a.sent();
                 _a.label = 6;
             case 6:
-                _i++;
-                return [3 /*break*/, 4];
+                if (!followingFeed.isMoreAvailable()) return [3 /*break*/, 10];
+                console.log('More available');
+                return [4 /*yield*/, new Promise(function (resolve) { return setTimeout(resolve, 120000); })];
             case 7:
+                _a.sent();
+                return [4 /*yield*/, followingFeed.items()];
+            case 8:
+                myFollowing_1 = _a.sent();
+                return [4 /*yield*/, muteFollower(myFollowing_1, ig, i)];
+            case 9:
+                _a.sent();
+                return [3 /*break*/, 6];
+            case 10:
+                //Mute recommendations
                 console.log('Done');
                 return [2 /*return*/];
         }
     });
 }); })();
+function muteFollower(myFollowing, ig, i) {
+    return __awaiter(this, void 0, void 0, function () {
+        var _loop_1, _i, myFollowing_2, user;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _loop_1 = function (user) {
+                        var time;
+                        return __generator(this, function (_b) {
+                            switch (_b.label) {
+                                case 0: return [4 /*yield*/, ig.friendship.mutePostsOrStoryFromFollow({ targetPostsAuthorId: user.pk.toString(), targetReelAuthorId: user.pk.toString() })];
+                                case 1:
+                                    _b.sent();
+                                    console.log("Muted ".concat(user.username));
+                                    time = Math.round(Math.random() * 6000) + 1000;
+                                    return [4 /*yield*/, new Promise(function (resolve) { return setTimeout(resolve, time); })];
+                                case 2:
+                                    _b.sent();
+                                    //Wait randomly doing 6 minutes another request
+                                    i++;
+                                    if (!(i % 50 === 0)) return [3 /*break*/, 4];
+                                    console.log('Waiting 2 minutes');
+                                    return [4 /*yield*/, new Promise(function (resolve) { return setTimeout(resolve, 120000); })];
+                                case 3:
+                                    _b.sent();
+                                    _b.label = 4;
+                                case 4: return [2 /*return*/];
+                            }
+                        });
+                    };
+                    _i = 0, myFollowing_2 = myFollowing;
+                    _a.label = 1;
+                case 1:
+                    if (!(_i < myFollowing_2.length)) return [3 /*break*/, 4];
+                    user = myFollowing_2[_i];
+                    return [5 /*yield**/, _loop_1(user)];
+                case 2:
+                    _a.sent();
+                    _a.label = 3;
+                case 3:
+                    _i++;
+                    return [3 /*break*/, 1];
+                case 4: return [2 /*return*/];
+            }
+        });
+    });
+}
--- script.ts
+++ script.ts
@@ -1,27 +1,45 @@
-console.log('Hello World!');
+console.log('Starting script');
 import 'dotenv/config';
-import { IgApiClient } from 'instagram-private-api';
+import { AccountFollowingFeedResponse, AccountFollowingFeedResponseUsersItem, IgApiClient } from 'instagram-private-api';
 
 const ig = new IgApiClient();
 ig.state.generateDevice(process.env.IG_USERNAME as string);
 
 
 ( async () => {
-    //Login
     await ig.simulate.preLoginFlow();
     const loggedInUser = await ig.account.login(process.env.IG_USERNAME as string , process.env.IG_PASSWORD as string);
     process.nextTick(async () => await ig.simulate.postLoginFlow());
+    ig.challenge.auto(true);
     //Get following
     const followingFeed = ig.feed.accountFollowing(loggedInUser.pk);
     const myFollowing = await followingFeed.items();
+    await new Promise(resolve => setTimeout(resolve, 120000));
     //Mute all
-    for (const user of myFollowing) {
-        await ig.friendship.mutePostsOrStoryFromFollow({ targetPostsAuthorId: user.pk.toString() , targetReelAuthorId: user.pk.toString()});
-        console.log(`Muted ${user.username}`);
-        const time = Math.round(Math.random() * 1000) + 200;
-        await new Promise(resolve => setTimeout(resolve, time));
+    let i: number = 0;
+    await muteFollower(myFollowing, ig,i);
+    while (followingFeed.isMoreAvailable()) {
+        console.log('More available');
+        await new Promise(resolve => setTimeout(resolve, 120000));
+        const myFollowing = await followingFeed.items();
+        await muteFollower(myFollowing, ig, i);
     }
     //Mute recommendations
     console.log('Done');
 })();
 
+
+async function muteFollower(myFollowing: AccountFollowingFeedResponseUsersItem[], ig: IgApiClient, i: number) {
+    for (const user of myFollowing) {
+        await ig.friendship.mutePostsOrStoryFromFollow({ targetPostsAuthorId: user.pk.toString() , targetReelAuthorId: user.pk.toString()});
+        console.log(`Muted ${user.username}`);
+        const time = Math.round(Math.random() * 6000) + 1000;
+        await new Promise(resolve => setTimeout(resolve, time));
+        //Wait randomly doing 6 minutes another request
+        i++;
+        if (i % 50 === 0) {
+            console.log('Waiting 2 minutes');
+            await new Promise(resolve => setTimeout(resolve, 120000));
+        }
+    }
+}
